753a5614653cae6f41dd9033ea675d9f30fd32df
Kamaal Farah <kamaal.f1@gmail.com>
2025-04-11 18:14:28 +0200

    Managing base64 editor value

--- apps/web/src/views/components/base64-editor/base64-editor.tsx
+++ apps/web/src/views/components/base64-editor/base64-editor.tsx
@@ -2,13 +2,31 @@ import { VStack, Text } from '@/react-ui';
 
 import './base64-editor.css';
 
-type Base64EditorProps = { title: string };
+type Base64EditorProps = {
+  title: string;
+  value: string;
+  onChange: (text: string) => void;
+  onFocus?: () => void;
+  onBlur?: () => void;
+};
 
-function Base64Editor({ title }: Base64EditorProps) {
+function Base64Editor({
+  title,
+  value,
+  onChange,
+  onBlur = () => {},
+  onFocus = () => {},
+}: Base64EditorProps) {
   return (
     <VStack frame={{ width: '80%' }} alignment="leading">
       <Text font="headline">{title}</Text>
-      <textarea className="editor"></textarea>
+      <textarea
+        className="editor"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        onBlur={onBlur}
+        onFocus={onFocus}
+      ></textarea>
     </VStack>
   );
 }
--- apps/web/src/views/pages/base64-decoding-page/base64-decoding-page.tsx
+++ apps/web/src/views/pages/base64-decoding-page/base64-decoding-page.tsx
@@ -1,13 +1,48 @@
+import React from 'react';
+
 import { VStack } from '@/react-ui';
 import Base64Editor from '@/views/components/base64-editor/base64-editor';
 
 import './base64-decoding-page.css';
 
+type TextFieldsMap = typeof TEXT_FIELDS;
+type TextFields = TextFieldsMap[keyof TextFieldsMap];
+
+const TEXT_FIELDS = { BASE64: 'base64', DECODED: 'decoded' } as const;
+
 function Base64DecodingPage() {
+  const [base64, setBase64] = React.useState('');
+  const [decoded, setDecoded] = React.useState('');
+  const [focusedField, setFocusedField] = React.useState<TextFields | null>(
+    null,
+  );
+
+  function handleBase64Change(text: string) {
+    if (focusedField !== TEXT_FIELDS.BASE64) return;
+
+    setBase64(text);
+  }
+
+  function handleDecodedChange(text: string) {
+    if (focusedField !== TEXT_FIELDS.DECODED) return;
+
+    setDecoded(text);
+  }
+
   return (
     <VStack frame={{ width: '100%' }}>
-      <Base64Editor title="Base64" />
-      <Base64Editor title="Decoded" />
+      <Base64Editor
+        title="Base64"
+        value={base64}
+        onChange={handleBase64Change}
+        onFocus={() => setFocusedField(TEXT_FIELDS.BASE64)}
+      />
+      <Base64Editor
+        title="Decoded"
+        value={decoded}
+        onChange={handleDecodedChange}
+        onFocus={() => setFocusedField(TEXT_FIELDS.DECODED)}
+      />
     </VStack>
   );
 }
