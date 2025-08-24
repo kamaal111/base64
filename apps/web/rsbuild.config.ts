71be4834db9f00d44a445ccd2d44d367a24f71ee
Kamaal Farah <kamaal.f1@gmail.com>
2025-08-24 19:46:46 +0200

    Adding Appcast

--- apps/web/rsbuild.config.ts
+++ apps/web/rsbuild.config.ts
@@ -4,4 +4,6 @@ import { pluginReact } from '@rsbuild/plugin-react';
 export default defineConfig(() => ({
   plugins: [pluginReact()],
   output: { assetPrefix: '/base64' },
+  server: { publicDir: { name: 'public', copyOnBuild: true } },
+  dev: { writeToDisk: true },
 }));
